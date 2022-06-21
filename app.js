af0b427e086230dece227f7a7ffeb19b08d7904d
sanjoy <sanjoykundu187@gamil.com>
2022-06-21 19:24:11 +0600

    testimonial part added

--- app.js
+++ app.js
@@ -17,3 +17,28 @@ function showSlides() {
 	dots[slideIndex - 1].className += " active";
 	setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+
+/* ====== slider=====*/
+if (document.getElementById("slider--text")) {
+	let slides = [
+		"<h2>Name: Jennifer Moreno</h2>",
+		"<p>I love my eyebrow design. I'm usually very picky about my eyebrows and not everyone can give me what I want. You are amazing. Thank you for the amazing job you’ve done, I’ll be recommending you to my friends from now on!</p>",
+
+		"<h2>Name: Sanjoy</h2>",
+		"<p>I love my eyebrow design. I'm usually very picky about my eyebrows and not everyone can give me what I want. You are amazing. Thank you for the amazing job you’ve done, I’ll be recommending you to my friends from now on!</p>"
+	];
+
+	let i = 0;
+
+	const slider = () => {
+		document.getElementById("slider--text").innerHTML = slides[i];
+		document.getElementById("slider--text").classList.add('fade-in');
+
+
+		(i < slides.length - 1) ? i++ : i = 0;
+	};
+
+	slider(); // Start slider immediately
+	setInterval(slider, 1000); // Slide every 4 seconds
+}
